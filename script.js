c3c46c81b7122890c874d818ea8e866a74018772
pinkikumari1 <106245030+pinkikumari1@users.noreply.github.com>
2023-04-12 13:30:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+// let abc;
+// abc ="hello pinki kumari";
+// abc=5;
+// // document.write(abc);
+// if(abc==5){
+//     console.log(abc);
+//     document.write(abc);
+// }document.write("\tpinki")
+
+// const abc="pinki";
+// const xyz="rinki";
+// xyz="kumari";
+// // abc=5;
+// document.write(abc);
+// document.write(xyz);
+
+// "use strict";
+// let n=1;
+// while (n<=10){
+//     document.write(n*2,"  ,");
+//     n++;
+// }
+
+// "use strict"
+// let n=1;
+// do{
+//     document.write(n*3," , ");
+//     n++;
+// }while(n<=10);
+
+// ********  for of ********
+
+// "use strict";
+// const arr =[2,4,6,8,10];
+// for(let ele of arr){
+//     document.write(ele,",");
+// }
+
+// "use strict";
+// const obji={Pinki:2002,Rinki:2001,Kiran:1998,Rakesh:1995};
+// for (let name in obji){
+//     document.write(name," Birth in : ",obji[name]," ");
+// }
+
+// "use strict"
+// function get_max(a,b){
+//     if (a>b){
+//         return a;
+//     }else{
+//         return b;
+//     }
+// }
+// let max =get_max(3,8);
+// document.write("Max Is : ",max);
+
+// "use strict"
+// function make_change(a,b,c){
+//     a=5;
+//     b=[1,3,5,7,9];
+//     c[0]=8;
+// }
+// let x=4;
+// let y=[2,4,6,8,10];
+// let z=[2,4,6,8,10];
+// make_change(x,y,z);
+// document.write(x);
+// document.write(y);
+// document.write(z);
+
+// "use strict"
+// let pinki=function(x,y){
+//     let p=x*y;
+// };
+// let r=pinki(5,10);
+// document.write(r);
+
+
+// Writing into an HTML element, using = (innerHTML , document.getElementById(id))
+// Writing into the HTML output using  = (document.write().)
+// Writing into an alert box, using  = (window.alert().)
+// Writing into the browser console, using = (console.log().)
+
+
+
